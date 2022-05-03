945b8f3977300b051999c8f79522f4c791c54e36
llanillo <llanillo4@hotmail.com>
2022-05-03 10:45:25 -0300

    Create Github.h

---
+++ Github.h
@@ -0,0 +1,25 @@
+/*
+*   -- Pasos para Git --
+*   -- Git Bash here en la carpeta --
+*   $ git config --global user.name “Su Nombre”
+*   $ git config --global user.email “su e-mail”
+*   1. echo "# NOMBRE-DEL-README" >> README.md
+*   2. git init
+*   3. git status
++   4. touch .gitignore
+*   5. git commit -m "primer commit"
+*   6. git branch -M main
+*   7. git remote add origin https://github.com/su_usuario/tp12022.git
+*
+*   -- Pasos para commit --
+*   1. git add .
+*   2. git commit -m "MENSAJE"
+*   2. git push -u origin main
+*
+*   git branch RAMA (Crea nueva rama)
+*   git checkout RAMA (Mueve a la nueva rama)
+*   git checkout master (Muevo a la rama master)
+*   git checkout -b RAMA (Crea y se mueve a la nueva rama)
+*   git merge RAMA1 -m “merge RAMA2” (Merge de ramas)
+*
+*/
